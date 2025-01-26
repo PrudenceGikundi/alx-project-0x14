4d8b9bee227392afc77db832a309b39d6d796412
PrudenceGikundi <prudencekendi101@gmail.com>
2025-01-26 12:28:47 +0300

    moviestask3

--- alx-movie-app/components/layouts/Layout.tsx
+++ alx-movie-app/components/layouts/Layout.tsx
@@ -1,7 +1,15 @@
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
 
-const Layout : React.FC=()=>{
-  return(
-    <h1>layout page</h1>
-  );
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  )
 }
+
 export default Layout;
